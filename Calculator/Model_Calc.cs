e642364f4655b9c9e33920847386253b4a18dd7e
ВЛАДИСЛАВ ВЛАДИСЛАВ <Vladislav290403@gmail.com>
2024-07-29 12:43:44 +0400

    Добавьте файлы проекта.

---
+++ Calculator/Model_Calc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Model_Calc : INotifyPropertyChanged
+    {
+        private string input_str;
+        private string result;
+
+        public string Input_str
+        { 
+            get { return input_str;  }
+            set 
+            { 
+                input_str = value;
+                OnPropertyChanged("Input_str");
+            }
+        }
+
+        public string Result { get {  return result; }  }
+
+        public void add_input(string str_for_add)
+        {
+            input_str += str_for_add;
+            Console.WriteLine(input_str);
+            OnPropertyChanged("Input_str");
+        }
+
+        public void SetResult()
+        {
+            result = new DataTable().Compute(input_str, null).ToString();
+            OnPropertyChanged("Result");
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Calculator/ViewModal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.RightsManagement;
+
+namespace Calculator
+{
+    public class ViewModal : INotifyPropertyChanged
+    {
+        private Model_Calc my_class;
+        private RelayCommand addInput;
+        private RelayCommand setResult;
+        public RelayCommand AddInput
+        {
+            get
+            {
+                
+                return addInput ??
+                  (addInput = new RelayCommand(obj =>
+                  {
+                      my_class.add_input("5+5");
+                  }));
+            }
+        }
+
+        public Model_Calc My_Class
+        {
+            get { return my_class; }
+            set
+            {
+                my_class = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+        public RelayCommand SetResult
+        {
+            get
+            {
+                return setResult ??
+                    (setResult = new RelayCommand(obj =>
+                    {
+                        my_class.SetResult();
+                    }));
+            }
+        }
+        public ViewModal()
+        {
+            my_class = new Model_Calc();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
